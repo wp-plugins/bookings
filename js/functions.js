ba5fb95aaccbf230c40e5c76d8a64760257471e8
erikpbogaerts <erikpbogaerts@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-10 11:31:34 +0000

    git-svn-id: https://plugins.svn.wordpress.org/bookings/trunk@517188 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/functions.js
+++ js/functions.js
@@ -903,6 +903,23 @@ function bookingsWindowClose() {
 	if ((bookingsDialog2 instanceof jQuery) && bookingsDialog2.dialog('isOpen')) bookingsDialog2.dialog('close');
 }
 
+Calendar = function () {
+};
+
+Calendar.setup = function (params) {
+	bookingsDialog.find('#'+params.button).hide();
+	params.daFormat=params.daFormat.replace('\%d','dd');
+	params.daFormat=params.daFormat.replace('\%m','mm');
+	params.daFormat=params.daFormat.replace('\%y','yy');
+	params.daFormat=params.daFormat.replace('\%Y','yy');
+	params.ifFormat=params.ifFormat.replace('%d','dd');
+	params.ifFormat=params.ifFormat.replace('%m','mm');
+	params.ifFormat=params.ifFormat.replace('%y','yy');
+	params.ifFormat=params.ifFormat.replace('%Y','yy');
+	bookingsDialog.find('#alt_'+params.inputField).datepicker({'dateFormat': params.daFormat, 'altField' : '#'+params.inputField, 'altFormat' : params.ifFormat});
+};
+
+
 jQuery(document).ready(function(){
 	jQuery(".spinonsubmit").click(function() {
 		jQuery(".spinner").show();
